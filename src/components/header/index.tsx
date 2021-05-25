7c3667eab5eb17447255bfea61c4f948facf4555
YuI <474766481@qq.com>
2021-05-26 02:24:29 +0800

    modify:修改 NavBar 样式

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,46 @@
 import React, {Component} from 'react';
 import Link from 'next/link';
+import tw from "tailwind-styled-components";
 
-interface Props {
+interface IProps {
 
 }
 
-class Header extends Component<Props, object> {
+interface IState {
+    containerActive: Boolean
+}
+
+interface ContainerProps {
+    $active: Boolean
+}
+
+const Container = tw.header<ContainerProps>`
+    absolute w-full
+    transition-all ease-in-out duration-500
+    ${(p) => p.$active ? 'top-4' : '-top-14'}
+`;
+
+const NavBar = tw.nav`
+    w-4/5 h-14 mx-auto px-6 rounded-xl text-black bg-white bg-opacity-70 flex justify-between items-center
+`;
+
+class Header extends Component<IProps, IState> {
+
+    state = {
+        containerActive: true
+    };
+
+    clickActive = () => {
+        const {containerActive} = this.state;
+        this.setState({containerActive: !containerActive});
+    };
+
     render() {
+        const {containerActive} = this.state;
+
         return (
-            <header className="absolute top-4 w-full">
-                <nav className="w-4/5 h-[60px] mx-auto px-6 rounded-full text-white bg-black bg-opacity-70 flex justify-between items-center">
+            <Container $active={containerActive}>
+                <NavBar>
                     <div>LOGO</div>
                     <ul className="flex">
                         <li className="pr-2"><Link href="/"><a>Home</a></Link></li>
@@ -18,9 +49,9 @@ class Header extends Component<Props, object> {
                         <li className="px-2"><Link href="/"><a>Portfolio</a></Link></li>
                         <li className="pl-2"><Link href="/"><a>Blog</a></Link></li>
                     </ul>
-                </nav>
-                <div className="w-20 h-[4px] bg-black rounded mt-2 mx-auto"/>
-            </header>
+                </NavBar>
+                <div className="w-20 h-1 bg-white rounded mt-2 mx-auto cursor-pointer" onClick={this.clickActive}/>
+            </Container>
         );
     }
 }
